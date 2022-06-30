628075e12ff408feb77347bfe9ebdf31750d20f4
Tiago Behenck <tiago.behenck.santos@gmail.com>
2022-06-29 23:14:29 -0300

    use the components using Context and Zustand to compare

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import ProfileContext from './components/Context/Profile';
+import ScoreCardContext from './components/Context/ScoreCard';
+import ProfileZustand from './components/Zustand/Profile';
+import ScoreCardZustand from './components/Zustand/ScoreCard';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Context API</h1>
+      <ProfileContext />
+      <ScoreCardContext />
+
+      <h1>Zustand</h1>
+      <ProfileZustand />
+      <ScoreCardZustand />
+    </div>
+  );
 }
 
 export default App;
